025888c6729ec06a0f23be8ae36c0930903a7a1b
HoènMike <hoanggg003@gmail.com>
2023-12-27 22:44:31 +0700

    Call it a day
    Done Summoning Pool

--- Assets/Codes/Spawner.cs
+++ Assets/Codes/Spawner.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+
+    public Transform[] spawnPoints;
+    float timer;
+
+    void Awake()
+    {
+        spawnPoints = GetComponentsInChildren<Transform>();
+    }
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
 
+        if (timer > 0.2f)
+        {
+            timer = 0f;
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, 2));
+        enemy.transform.position = spawnPoints[Random.Range(1, spawnPoints.Length)].position;
     }
 }
