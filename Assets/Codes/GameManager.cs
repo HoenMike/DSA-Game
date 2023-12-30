77eda08b14d7113763ffb1b5403886c6c03d1c4a
HoènMike <hoanggg003@gmail.com>
2023-12-30 22:59:35 +0700

    update itch.io version

--- Assets/Codes/GameManager.cs
+++ Assets/Codes/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour
     public int level;
     public int kill;
     public int exp;
-    public int[] nextExp = { 3, 5, 10, 100, 150, 210, 280, 360, 450, 600 }; //using queue
+    public int[] nextExp = { 3, 5, 10, 100, 150, 210, 280, 360, 450, 600 };
 
     [Header("# Game Objects")]
 
